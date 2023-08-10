d74975915ceba5c5a4c9f16583fd386815fa30d0
Yoshiya Hinosawa <stibium121@gmail.com>
2023-08-10 16:19:04 +0900

    feat: add post command

---
+++ post.ts
@@ -0,0 +1,20 @@
+// Copyright 2023 Yoshiya Hinosawa. All rights reserved. MIT license.
+
+import { ensureDir } from "std/fs/ensure_dir.ts";
+import { join } from "std/path/mod.ts";
+import { Temporal } from "esm/@js-temporal/polyfill@0.4.4";
+import { encode } from "x/bijective_base_n@v0.1.0/mod.ts";
+
+const n = Temporal.Now.zonedDateTimeISO();
+const dir = `tl/${n.year}${n.month.toString().padStart(2, "0")}`;
+await ensureDir(dir);
+const file = join(dir, `${encode([...Deno.readDirSync(dir)].length + 1)}.md`);
+Deno.writeTextFileSync(
+  file,
+  `---
+date: ${n}
+---
+...
+`,
+);
+new Deno.Command("code", { args: [file] }).outputSync();
