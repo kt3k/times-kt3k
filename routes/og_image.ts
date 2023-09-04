b331c7eeb0cef6934a5555a03c490f93d31e7731
Yoshiya Hinosawa <stibium121@gmail.com>
2023-09-04 17:38:32 +0900

    feat: return 304 for og images

--- routes/og_image.ts
+++ routes/og_image.ts
@@ -1,14 +1,29 @@
 // Copyright 2023 Yoshiya Hinosawa. All rights reserved. MIT license.
 
 import { RouteConfig, RouteContext } from "fresh/server.ts";
+import { encode } from "std/encoding/base64.ts";
 
 const ogImageU8 = await Deno.readFile("static/og-image.png");
 
-const imageHeader = { "Content-Type": "image/png" };
+const sharedEtag = `"${
+  encode(
+    new Uint8Array(
+      await crypto.subtle.digest(
+        "SHA-256",
+        new TextEncoder().encode(Deno.env.get("DENO_DEPLOYMENT_ID") ?? ""),
+      ),
+    ),
+  )
+}"`;
+
+const imageHeader = { "Content-Type": "image/png", "Etag": sharedEtag };
 
 const defaultResponse = new Response(ogImageU8, { headers: imageHeader });
 
-export async function handler(_req: Request, ctx: RouteContext) {
+export async function handler(req: Request, ctx: RouteContext) {
+  if (req.headers.get("if-none-match")?.includes(sharedEtag)) {
+    return new Response("", { status: 304 });
+  }
   try {
     await Deno.lstat(`tl/${ctx.params[0]}/image.png`);
     return new Response(
