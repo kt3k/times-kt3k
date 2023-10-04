09657f97a1466e6950f162d0e9355394e2564589
Yoshiya Hinosawa <stibium121@gmail.com>
2023-10-04 12:13:30 +0900

    chore: change github link

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@
 export default function Footer() {
   return (
     <footer class="text-xs py-5 text-gray-400 text-center">
-      <a href="https://github.com/kt3k">github</a>・
+      <a href="https://github.com/kt3k/times-kt3k">github</a>・
       <a href="https://kt3k.org">kt3k.org</a>
     </footer>
   );
