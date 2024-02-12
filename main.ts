89325db07346d8f96b915e32cc2ba43009b95cf7
Yoshiya Hinosawa <stibium121@gmail.com>
2024-02-12 18:31:44 +0900

    chore: add image for 2024-02

--- main.ts
+++ main.ts
@@ -13,8 +13,4 @@ import manifest from "./fresh.gen.ts";
 import twindPlugin from "fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
 
-Deno.cron("sample cron", "0 0 * * *", () => {
-  console.log("hi");
-});
-
 await start(manifest, { plugins: [twindPlugin(twindConfig)] });
